e8c6920ef2a3a0690152b63192ee6b52dbb68baf
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2023-01-16 23:45:45 +0100

    upd

---
+++ src/components/List/List.jsx
@@ -0,0 +1,32 @@
+import propTypes from 'prop-types';
+import css from './List.module.css';
+
+export const List = ({ contacts, handleDelete }) => (
+  <div className={css.wraperContactList}>
+    <ul className={css.contactList}>
+      {contacts.map((contact, id) => (
+        <li key={id} className={css.contactListItem}>
+          {contact.name}: {contact.number}
+          <button
+            type="button"
+            className={css.contactListItemBtn}
+            onClick={() => handleDelete(contact.id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+List.propTypes = {
+  contacts: propTypes.arrayOf(
+    propTypes.exact({
+      id: propTypes.string.isRequired,
+      name: propTypes.string.isRequired,
+      number: propTypes.string.isRequired,
+    })
+  ),
+  handleDelete: propTypes.func.isRequired,
+};
